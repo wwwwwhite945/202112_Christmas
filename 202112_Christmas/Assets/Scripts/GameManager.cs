b1d7e5eb9ac10677d444476a296f3a28d1a129c1
wwwwwhite <a0922763977@gmail.com>
2022-04-19 11:29:43 +0800

    change files position

---
+++ 202112_Christmas/Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("設定&遊玩方法畫面")]
+    public GameObject HowtoPlayScene; //HowtoPlay畫面
+    public GameObject SettingScene; //Setting畫面
+
+    [Space(10)]
+    [Header("音樂控制")]
+    public Image musicImg;      //音樂開關
+    public Sprite[] musicSpr;   //開&關圖片
+    public AudioSource audioSource;
+    public GameObject audioObj;
+    static bool isAudioPlay = true; //確認目前是否有正在播放的AudioSource，true表示目前"有"播放中的AudioSource
+    bool isMusic = true;            //確認目前是否正在播放中
+
+    void Start()
+    {
+        Screen.SetResolution(/*螢幕寬度*/1920,/*螢幕高度*/ 1080, /*是否全屏顯示*/true); //控制畫面大小及比例
+
+        if (isAudioPlay)
+        {
+            DontDestroyOnLoad(audioObj);    //遊戲中不刪除
+            isAudioPlay = false;            //避免第二次進入場景時與DontDestroy衝突
+            audioSource.Play();
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GameStart() //開始遊戲
+    {
+        SceneManager.LoadScene("GameStart");
+    }
+
+
+    public void HowtoPlay() //打開HowtoPlay畫面
+    {
+        HowtoPlayScene.SetActive(true);
+    }
+    public void CloseHowtoPlay() //關閉HowtoPlay畫面
+    {
+        HowtoPlayScene.SetActive(false);
+    }
+
+    public void Setting() //打開Setting畫面
+    {
+        SettingScene.SetActive(true);
+    }
+    public void CloseSetting() //關閉Setting畫面
+    {
+        SettingScene.SetActive(false);
+    }
+
+    public void MusicControl() //音樂控制
+    {
+        if (isMusic == true)
+        {
+            isMusic = false;
+            musicImg.sprite = musicSpr[0];
+            audioSource.Pause();
+            Debug.Log("pause music now.");
+        }
+        else
+        {
+            isMusic = true;
+            musicImg.sprite = musicSpr[1];
+            audioSource.Play();
+            Debug.Log("play music now.");
+        }
+    }
+
+    public void QuitGame() //結束遊戲
+    {
+        Application.Quit();
+    }
+}
---
+++ 202112_Christmas/Assets/Scripts/Tree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tree : MonoBehaviour {
+
+
+    Rigidbody2D rb;
+    public float jump;  //物件跳躍高度
+    bool isJumping;     //是否正在跳躍
+    public GameControl gc; //引用"GameControl"腳本
+
+    [Space(10)]
+    [Header("物件圖片")]
+    public Image treeImg;
+    public Sprite[] treeSpr;
+
+    void Start()
+    {
+        treeImg.sprite = treeSpr[0];
+        rb = GetComponent<Rigidbody2D>();
+        isJumping = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping == false) //當按下空白鍵且非正在跳躍
+        {
+            rb.velocity = new Vector2(0, jump); //rigidbody的線速度，單位是每秒
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+        if (collision.gameObject.tag == "Gift")
+        {
+            treeImg.sprite = treeSpr[1];
+            gc.GameOver(); //撞到即輸 //引用"GameControl"腳本中的"GameOver()"
+        }
+    }
+
+}
